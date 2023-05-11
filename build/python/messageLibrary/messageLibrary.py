6ba82e9d378360a67f4281feaba1949a0136e084
Mohammad Rahman <mcmogaming@gmail.com>
2023-05-11 15:26:01 -0400

    turn a into test5

--- build/python/messageLibrary/messageLibrary.py
+++ build/python/messageLibrary/messageLibrary.py
@@ -8,7 +8,7 @@ name = "Message Library"
 
 def replace_letters(message):
     letter_target = 'a'
-    letter_replacement = 'test4'
+    letter_replacement = 'test5'
     result = ""
     for i in range(0, len(message)):
         if message[i] == letter_target:
